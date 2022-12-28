59c4649a2c1b95c4fd96478217066d91cfea2372
V1ktorkk <ya-viktor44444@yandex.ru>
2022-12-28 18:08:44 +0300

    Add: lab6.1

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -7,21 +7,35 @@ function renderInterests() {
     const interestContainer = document.getElementById("interest-container")
     let newContent = ""
     for (let interest in interests) {
-        newContent += "<div class=\"interest-container__interest\">"+ interests[interest] +"</div>"
+        newContent += "<div class=\"interest-container__interest\">"+ interests[interest].value + "<br>" + normalDate(new Date((interests[interest].date))) +"</div>"
     }
     interestContainer.innerHTML = newContent
 }
-
+function isNormal(str) {
+    if (str.trim() === '')
+        return false;
+    return true;
+}
 function submitInterest(event) {
     event.preventDefault()
     const interestField = document.getElementById("interest-field")
-    if (interestField.value !== "") {
-        interests.push(interestField.value)
+    let obj = {
+
+        value: interestField.value,
+        date: new Date().getTime()
+    }
+    if (isNormal(interestField.value)) {
+        interests.push(obj)
         localStorage.setItem("interests", JSON.stringify(interests))
         console.log(localStorage.getItem("interests"))
         interestField.value = ""
         renderInterests()
     }
+}
+
+
+function normalDate(date) {
+    return `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}  ${date.getHours()}:${date.getMinutes()}`
 }
 
 function ready() {
--- scripts/fetching.js
+++ scripts/fetching.js
@@ -1,26 +1,43 @@
 
 function fetchNews() {
-    fetch("https://jsonplaceholder.typicode.com/albums/1/photos")
+    const url = 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI' +
+        '?q=shoes' +
+        '&pageNumber=1' +
+        '&pageSize=10' +
+        '&autoCorrect=true';
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'fe688d8b33mshcdbd95b6dfb172ep1f43d2jsn911fc7e94992',
+            'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
+        }
+    };
+
+
+    fetch(url, options)
         .then(response => {
             if (response.status === 200) {
                 return response.json();
             }
             else {
                 document.getElementById("fetching__error").style.display = "block";
-                throw new Error("Something went wrong on API server!");
+                document.getElementById("preloader").style.display = "none";
+                throw new Error("Something went wrong with API!")
             }
         })
-        .then(news => {
+        .then(response => {
             document.getElementById("preloader").style.display = "none";
-            console.log(news);
-            drawNews(news.filter(item => item.id > Math.floor(Math.random() * 50)));
+            console.log(response.value)
+            drawNews(response.value.filter((item, index) => index > Math.floor(Math.random() * 10)))
         })
         .catch(error => console.error(error));
 }
 
 function drawNews(news) {
+    console.log(news)
     let innerContent = "";
-    for (let i = 0; i < news.length - 20; i = i + 2) {
+    for (let i = 0; i < news.length - 1; i = i + 2) {
         innerContent += `<div class="news">
                             <div class = "item">
                                 <p> ${news[i].title} </p>
@@ -34,6 +51,7 @@ function drawNews(news) {
     }
     document.getElementById("news_wrapper").innerHTML += innerContent;
 }
+
 
 function ready() {
     fetchNews();
